c0cfe9a74baa4750a2f6c78ad95e542ecbb59ab0
F0XShabong <153483080+F0XShabong@users.noreply.github.com>
2023-12-20 12:56:29 +0530

    added advance patrol function

--- Assets/Scripts/EnemiesScripts/Goblins/MeleeGoblin.cs
+++ Assets/Scripts/EnemiesScripts/Goblins/MeleeGoblin.cs
@@ -5,35 +5,80 @@ using UnityEngine;
 public class MeleeGoblin :PlayerDetector
 {
     private Animator anim;
-  
+
     [SerializeField] private float attackRange;
     [SerializeField] private Transform attackPoint;
     private bool isAttacking = false;
-    [SerializeField] private float attackCooldown = 2f; 
+    [SerializeField] private float attackCooldown = 2f;
     private float currentCooldown = 0f;
     public LayerMask playerLayer;
     [SerializeField] private float damageAmount;
+
+    [Header("Patrol Settings")]
+    [SerializeField] private Transform leftPoint;
+    [SerializeField] private Transform rightPoint;
+    [SerializeField] private float patrolSpeed = 2f;
+    private bool isPatrolling = true;
+
     private void Awake()
     {
         anim = GetComponent<Animator>();
     }
+
     private void Update()
     {
         if (PlayerDetected)
         {
-            if (!isAttacking && currentCooldown <= 0)
-            {
-                AttackPlayer();
-            }
-            else
-            {
-                currentCooldown -= Time.deltaTime;
-            }
+            StopPatrolAndAttack();
         }
         else
         {
-            DontAttackPlayer();
+            if (isPatrolling)
+            {
+                Patrol();
+            }
+            else
+            {
+                DontAttackPlayer();
+            }
         }
+    }
+
+    private void Patrol()
+    {
+        // Patrol logic
+        transform.Translate(Vector2.right * patrolSpeed * Time.deltaTime);
+        anim.SetFloat("moveSpeed", Mathf.Abs(patrolSpeed));
+
+      
+        if ((patrolSpeed > 0 && transform.position.x >= rightPoint.position.x) ||
+            (patrolSpeed < 0 && transform.position.x <= leftPoint.position.x))
+        {
+            Flip();
+        }
+    }
+
+    private void StopPatrolAndAttack()
+    {
+        isPatrolling = false;
+        anim.SetFloat("moveSpeed", 0f);
+
+        if (!isAttacking && currentCooldown <= 0)
+        {
+            AttackPlayer();
+        }
+        else
+        {
+            currentCooldown -= Time.deltaTime;
+        }
+    }
+
+    private void Flip()
+    {
+        patrolSpeed *= -1;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 
     private IEnumerator AttackCooldown()
@@ -52,24 +97,25 @@ public class MeleeGoblin :PlayerDetector
 
     public void DontAttackPlayer()
     {
+        isPatrolling = true;
         isAttacking = false;
     }
 
     public void DamagePlayer()
     {
-
-
         Collider2D[] hittarget = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, playerLayer);
 
         foreach (Collider2D target in hittarget)
         {
-
             target.GetComponent<Health>().TakeDamage(damageAmount);
         }
     }
+
     void OnDrawGizmosSelected()
     {
-
         Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+
+        // Draw lines between patrol points
+        Gizmos.DrawLine(leftPoint.position, rightPoint.position);
     }
 }
