653a679ae3242bc9694486831ae2e5017bdb2515
F0XShabong <153483080+F0XShabong@users.noreply.github.com>
2023-12-22 18:36:07 +0530

    added spider script

--- Assets/Scripts/EnemiesScripts/Spider/Spider.cs
+++ Assets/Scripts/EnemiesScripts/Spider/Spider.cs
@@ -5,11 +5,34 @@ using UnityEngine;
 public class Spider : PlayerDetector
 {
     private Animator anim;
-
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    private bool isGround;
+    private Rigidbody2D spiderRb;
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
+        spiderRb = GetComponent<Rigidbody2D>();
+
+    }
+    private void Update()
+    {
+        if (PlayerDetected)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+    private void JumpAttack()
+    {
+
+    }
+    private void StopJumpAttack()
+    {
 
     }
 }
