68da5782ef4792bba9e203a38b91c962fe053257
F0XShabong <153483080+F0XShabong@users.noreply.github.com>
2023-12-26 14:53:34 +0530

    added MainMenu scene

--- Assets/Scripts/UIScripts/GameOver.cs
+++ Assets/Scripts/UIScripts/GameOver.cs
@@ -5,16 +5,17 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class GameOver : MonoBehaviour
 {
-    public GameObject gameOverScreen;
-
-    public void CallGameOverScreen()
-    {
-        gameOverScreen.SetActive(true);
-    }
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
-
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
   
 }
