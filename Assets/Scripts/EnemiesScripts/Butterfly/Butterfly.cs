7f99d3d492415f5f1e2c9292e5a5f240177f9c5f
qaz_xsw <qazwsxxswzaq0192837465@gmail.com>
2023-12-26 13:56:06 +0530

    added simple anim

--- Assets/Scripts/EnemiesScripts/Butterfly/Butterfly.cs
+++ Assets/Scripts/EnemiesScripts/Butterfly/Butterfly.cs
@@ -7,12 +7,16 @@ public class Butterfly : PlayerDetector
     [Header("Butterfly Parameters")]
     [SerializeField] private EnemyPatrol enemyPatrol;
     [SerializeField] private GameObject playerGameObject;
+    [SerializeField] private float chaseDistance = 0.5f;
     private SpriteRenderer sprite;
+    private Animator beeAnim;
     private Rigidbody2D rb;
+    private bool playerCaught = false;
 
     private void Awake() {
         sprite = this.GetComponent<SpriteRenderer>();
         rb = this.GetComponent<Rigidbody2D>();
+        beeAnim = this.GetComponent<Animator>();
     }
 
     private void Update() {
@@ -23,6 +27,7 @@ public class Butterfly : PlayerDetector
                 enemyPatrol.patrol = false;
             }
             ChasePlayer();
+            LaunchAttack();
         }
         else
         {
@@ -35,10 +40,33 @@ public class Butterfly : PlayerDetector
         Vector2 direction = DirectionToTarget.normalized; 
         rb.velocity = direction * (enemyPatrol.moveSpeed * 1.3f);
 
-        if (Vector2.Distance(playerGameObject.transform.position, transform.position) < 0.5f)
+        if (Vector2.Distance(playerGameObject.transform.position, transform.position) < chaseDistance)
         {
             rb.velocity = Vector2.zero;
+            playerCaught = true;
             return;
+        }
+    }
+
+    void LaunchAttack()
+    {
+        if (playerCaught)
+        {
+            if (enemyPatrol.IsFacingRight())
+                beeAnim.SetBool("attack", true);
+            else
+                beeAnim.SetBool("attack", true);
+        }
+    }
+
+    void EndAttack()
+    {
+        if (playerCaught)
+        {
+            // play attack anim
+            // hurt player
+            beeAnim.SetBool("attack", false);
+            playerCaught = false;
         }
     }
 }
