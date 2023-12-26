ad31947314c6e302edc4250385e3a137af678a8c
F0XShabong <153483080+F0XShabong@users.noreply.github.com>
2023-12-26 22:04:06 +0530

    added audio manager script

--- Assets/Scripts/HealthScript/HealthPickup.cs
+++ Assets/Scripts/HealthScript/HealthPickup.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Serialization;
 public class HealthPickup : MonoBehaviour
 {
-    [SerializeField] private float healingvalue;
- 
-    private void OnTriggerEnter2D(Collider2D collision)
+    [SerializeField] private float healingValue;
+    [SerializeField] private AudioClip healSound;
+       private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
-            collision.GetComponent<Health>().AddHealth(healingvalue);
-            gameObject.SetActive(false);
+            collision.GetComponent<Health>().AddHealth(healingValue);
+            AudioManager.instance.PlaySoundEffects(healSound);
+            Destroy(this.gameObject);
         }
     }
 }
