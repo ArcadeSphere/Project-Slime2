e740713293ce5a79d5842b6f6a8d1ac2a5b8747e
qaz_xsw <qazwsxxswzaq0192837465@gmail.com>
2023-12-27 14:39:35 +0530

    added checkpoint anim

--- Assets/Scripts/Checkpoint/CheckpointHandler.cs
+++ Assets/Scripts/Checkpoint/CheckpointHandler.cs
@@ -1,13 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CheckpointHandler : MonoBehaviour
 {
     private bool isActivated = false;
+    [SerializeField] private GameObject explode;
+    [SerializeField] private GameObject text;
+    [SerializeField] private String displayText;
+    private Animator explodeAnim;
+    private Animator textAnim;
 
     private void Awake() {
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start() {
+        if (displayText != "")
+            text.GetComponent<TextMeshPro>().text = displayText;
+        else
+            text.GetComponent<TextMeshPro>().text = "Default Text";
+        if (explode != null && text != null)
+        {
+            explodeAnim = explode.GetComponent<Animator>();
+            textAnim = text.GetComponent<Animator>();
+        }
+        
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
@@ -17,6 +38,11 @@ public class CheckpointHandler : MonoBehaviour
             {
                 isActivated = true;
                 PlayerData.Instance.currentCheckpoint = this.transform;
+                if (explode != null && text != null)
+                {
+                    explodeAnim.SetTrigger("triggered");
+                    textAnim.SetTrigger("textfade");
+                }
                 Debug.Log("Checkpoint Set");
             }
         }
