57f6401fbf9c7a7171baf5d79c9aeebd48892f99
F0XShabong <153483080+F0XShabong@users.noreply.github.com>
2024-01-04 17:55:55 +0530

    added bouncing shroom

--- Assets/Scripts/IEScripts/BouncePad.cs
+++ Assets/Scripts/IEScripts/BouncePad.cs
@@ -6,16 +6,24 @@ public class BouncePad : MonoBehaviour
 {
     [SerializeField] private float bounceForce = 20f;
     [SerializeField] private AudioClip bounceSound;
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();   
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            anim.SetTrigger("Bounce");
             AudioManager.instance.PlaySoundEffects(bounceSound);
             PlayerMovement playerMovement = collision.gameObject.GetComponent<PlayerMovement>();
 
             // Check if the player is not currently dashing or jumping
             if (!playerMovement.IsDashing() && !playerMovement.IsJumping())
             {
+
                 collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);
             }
         }
