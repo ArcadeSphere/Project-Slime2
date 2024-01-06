e32ab9d05767a024e6ec18eb5fb22db816358254
F0XShabong <153483080+F0XShabong@users.noreply.github.com>
2024-01-06 15:27:01 +0530

    added options menu in main menu

--- Assets/Scripts/UIScripts/MainMenu.cs
+++ Assets/Scripts/UIScripts/MainMenu.cs
@@ -6,6 +6,31 @@ public class MainMenu : MonoBehaviour
 {
     [SerializeField] private PlayerCombat playerCombat;
     [SerializeField] private PlayerMovement playerMove;
+    public GameObject optionsMenuUI;
+    public GameObject volumeMenuUI;
+    public GameObject keybindingsMenuUI;
+
+    private enum MainMenuState
+    {
+        Main,
+        Options,
+        Volume,
+        Keybindings
+    }
+
+    private MainMenuState currentState = MainMenuState.Main;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (currentState == MainMenuState.Options || currentState == MainMenuState.Volume || currentState == MainMenuState.Keybindings)
+            {
+                BackToMainMenu();
+            }
+        }
+    }
+
     public void PlayGame()
     {
         playerCombat.enabled = true;
@@ -13,8 +38,35 @@ public class MainMenu : MonoBehaviour
         Time.timeScale = 1f;
         SceneManager.LoadScene(1);
     }
+
     public void QuitTheGame()
     {
         Application.Quit();
+    }
+
+    public void OpenOptions()
+    {
+        currentState = MainMenuState.Options;
+        optionsMenuUI.SetActive(true);
+    }
+
+    public void OpenVolume()
+    {
+        currentState = MainMenuState.Volume;
+        volumeMenuUI.SetActive(true);
+    }
+
+    public void OpenKeybindings()
+    {
+        currentState = MainMenuState.Keybindings;
+        keybindingsMenuUI.SetActive(true);
+    }
+
+    public void BackToMainMenu()
+    {
+        currentState = MainMenuState.Main;
+        optionsMenuUI.SetActive(false);
+        volumeMenuUI.SetActive(false);
+        keybindingsMenuUI.SetActive(false);
     }
 }
