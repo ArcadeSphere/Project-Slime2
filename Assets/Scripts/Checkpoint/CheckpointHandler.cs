43a6061af8b717b3fb6b20303eb033d19de19ecf
qaz_xsw <qazwsxxswzaq0192837465@gmail.com>
2024-01-07 14:31:49 +0530

    renamed checkpoint animations

--- Assets/Scripts/Checkpoint/CheckpointHandler.cs
+++ Assets/Scripts/Checkpoint/CheckpointHandler.cs
@@ -9,8 +9,8 @@ public class CheckpointHandler : MonoBehaviour
 {
     private readonly String playerPrefKey = "IsActive";
     private bool isActivated;
-    [SerializeField] private GameObject explode;
-    [SerializeField] private GameObject text;
+    [SerializeField] private GameObject checkedAnimation;
+    [SerializeField] private GameObject textAnimation;
     [SerializeField] private String displayText;
     private Animator explodeAnim;
     private Animator textAnim;
@@ -22,13 +22,13 @@ public class CheckpointHandler : MonoBehaviour
     private void Start() {
         LoadValue();
         if (displayText != "")
-            text.GetComponent<TextMeshPro>().text = displayText;
+            textAnimation.GetComponent<TextMeshPro>().text = displayText;
         else
-            text.GetComponent<TextMeshPro>().text = "Default Text";
-        if (explode != null && text != null)
+            textAnimation.GetComponent<TextMeshPro>().text = "Default Text";
+        if (checkedAnimation != null && textAnimation != null)
         {
-            explodeAnim = explode.GetComponent<Animator>();
-            textAnim = text.GetComponent<Animator>();
+            explodeAnim = checkedAnimation.GetComponent<Animator>();
+            textAnim = textAnimation.GetComponent<Animator>();
         }
     }
 
@@ -40,7 +40,7 @@ public class CheckpointHandler : MonoBehaviour
                 isActivated = true;
                 SaveValue();
                 PlayerData.Instance.currentCheckpoint = this.transform;
-                if (explode != null && text != null)
+                if (checkedAnimation != null && textAnimation != null)
                 {
                     explodeAnim.SetTrigger("triggered");
                     textAnim.SetTrigger("textfade");
