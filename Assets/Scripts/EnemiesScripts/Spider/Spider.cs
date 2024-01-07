37b09d7da55395629a855268113b639c21dcc404
F0XShabong <153483080+F0XShabong@users.noreply.github.com>
2024-01-07 21:16:20 +0530

    added character flip

--- Assets/Scripts/EnemiesScripts/Spider/Spider.cs
+++ Assets/Scripts/EnemiesScripts/Spider/Spider.cs
@@ -20,26 +20,29 @@ public class Spider : PlayerDetector
     [SerializeField] private float SpiderJumpDelay = 1.5f;
     private float nextJumpTime;
     [SerializeField] private float damageAmount;
-    private bool isGround;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+
+    [Header("Reference Settings")]
+    [SerializeField] private CharacterFlip characterFlip;
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
         spiderRb = GetComponent<Rigidbody2D>();
+        characterFlip = GetComponent<CharacterFlip>();
         nextJumpTime = Time.time + SpiderJumpDelay;
     }
 
     private void Update()
     {
-        isGround = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+        bool isGround = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
 
         if (PlayerDetected)
         {
             if (isGround && Time.time >= nextJumpTime)
             {
-                FlipTowardsPlayer();
+                characterFlip.FlipTowardsTarget(playerTransform.position);
                 detectionIndicator.ActivateAlert();
                 StartCoroutine(DelayedSpiderJump());
                 nextJumpTime = Time.time + jumpCooldown;
@@ -51,50 +54,20 @@ public class Spider : PlayerDetector
         }
     }
 
-    //adds a delay for the first jump
+    // adds a delay for the first jump
     private IEnumerator DelayedSpiderJump()
     {
         yield return new WaitForSeconds(SpiderJumpDelay);
         JumpAttack();
     }
 
-    //jump attack
+    // jump attack
     private void JumpAttack()
     {
         anim.SetTrigger("JumpAttack");
         AudioManager.instance.PlaySoundEffects(attackSound);
         float distanceFromPlayer = playerTransform.position.x - transform.position.x;
         spiderRb.AddForce(new Vector2(distanceFromPlayer, jumpForce), ForceMode2D.Impulse);
-    }
-
-    private void FlipTowardsPlayer()
-    {
-        if (transform.position.x < playerTransform.position.x)
-        {
-            if (IsFacingRight())
-            {
-                SpiderFlip();
-            }
-        }
-        else
-        {
-            if (!IsFacingRight())
-            {
-                SpiderFlip();
-            }
-        }
-    }
-
-    private bool IsFacingRight()
-    {
-        return transform.localScale.x > 0;
-    }
-
-    private void SpiderFlip()
-    {
-        Vector3 scale = transform.localScale;
-        scale.x *= -1;
-        transform.localScale = scale;
     }
 
     public void SpiderDamagePlayer()
