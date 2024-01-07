927c6bc1c468794f44e2757a85bd391051349884
F0XShabong <153483080+F0XShabong@users.noreply.github.com>
2024-01-07 19:30:12 +0530

    added a detectionindicator script

---
+++ Assets/Scripts/AlertScript/DetectionIndicator.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionIndicator : MonoBehaviour
+{
+    [SerializeField] private GameObject indicatorObject;
+    private bool hasPlayedSound = false;
+    [SerializeField] private AudioClip alertSound;
+
+    private void Start()
+    {
+        if (indicatorObject != null)
+        {
+            indicatorObject.SetActive(false);
+        }
+    }
+
+    public void Activate()
+    {
+        SetIndicatorActive(true);
+        PlayAlertSound();
+    }
+
+    public void Deactivate()
+    {
+        SetIndicatorActive(false);
+        hasPlayedSound = false;
+    }
+
+    private void SetIndicatorActive(bool isActive)
+    {
+        if (indicatorObject != null)
+        {
+            indicatorObject.SetActive(isActive);
+        }
+    }
+
+    private void PlayAlertSound()
+    {
+        if (indicatorObject != null && !hasPlayedSound)
+        {
+            AudioManager.instance.PlaySoundEffects(alertSound);
+            hasPlayedSound = true;
+        }
+    }
+}
